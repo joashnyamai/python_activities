02f82d0f05d9989138e4a4963f92e989298154ed
joashnyamai <105638207+joashnyamai@users.noreply.github.com>
2024-09-22 09:56:55 +0300

    Add files via upload

---
+++ #python data types.py
@@ -0,0 +1,22 @@
+#python data types
+#fruits = []
+#while True:
+   #values = input("Enter values:")
+   #if values == "stop":
+     #break
+   #fruits.append(values)
+   #fruits.sort()
+   #print(fruits)
+#python data types
+fruits = []
+while True:
+   values = input("Enter values: ")
+   if values == "stop":
+     break
+   fruits.append(values)
+   fruits.sort()
+   print(fruits)
+
+# Print the total number of values
+print("Total number of values: ", len(fruits))
+   
